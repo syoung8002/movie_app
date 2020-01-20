e70a57aed3b33f4680b6b1d8213244b5b9da21c6
dhtns818 <dhtns818@naver.com>
2020-01-20 11:45:10 +0900

    Updates

---
+++ precache-manifest.3ef60a79b8be10e5f1ca77ee9cb670e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c882b91a7327eb344c7cceaf356846ab",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "19892e661cf8efcedabc",
+    "url": "/movie_app/static/css/main.192766d0.chunk.css"
+  },
+  {
+    "revision": "53d03bd799dfb0f79c3c",
+    "url": "/movie_app/static/js/2.da3ff8c7.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/movie_app/static/js/2.da3ff8c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19892e661cf8efcedabc",
+    "url": "/movie_app/static/js/main.9aa46da1.chunk.js"
+  },
+  {
+    "revision": "b03db121bc0725d553bd",
+    "url": "/movie_app/static/js/runtime-main.4970d62d.js"
+  }
+]);
